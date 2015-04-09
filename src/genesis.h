add8bd96ef941f634e987bc924d152094fe2d713
mammix2 <mammix2@hotmail.com>
2015-04-09 21:39:35 +0100

    Tele

---
+++ src/genesis.h
@@ -0,0 +1,14 @@
+#ifndef GENESIS_H
+#define GENESIS_H
+
+#include "bignum.h"
+
+// Genesis block 0 hash
+
+static const uint256 hashGenesisBlock("000007780fbf6b013a4713df08cdadab1d045635d8e4e750d0e336d65fa172a7");
+static const uint256 hashGenesisBlockTestNet("000007780fbf6b013a4713df08cdadab1d045635d8e4e750d0e336d65fa172a7");
+
+// TODO: Move Kernel checksum in here
+
+
+#endif // GENESIS_H
